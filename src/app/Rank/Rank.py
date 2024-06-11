6b02253a9aa41507a3190800a2be9681f47c280c
zzc <2278860740@qq.com>
2024-06-12 00:02:07 +0800

    新增了排名功能

--- src/app/Rank/Rank.py
+++ src/app/Rank/Rank.py
@@ -4,6 +4,7 @@ from sqlalchemy import text
 from ..Utils import toJSON
 from ..DB_models.models import Students
 from flask_jwt_extended import jwt_required, get_jwt_identity
+from ..Utils.FormatConversion import toDataFrame
 
 # 创建路由蓝图
 rank_blue = Blueprint('rank', __name__)
@@ -17,8 +18,7 @@ def get_rank():
                     SELECT
                         student_id AS username,
                         COUNT(submission_id) AS submission_count,
-                        SUM(CASE WHEN status = 'Accepted' THEN 1 ELSE 0 END) / COUNT(submission_id) AS pass_rate,
-                        RANK() OVER (ORDER BY SUM(CASE WHEN status = 'Accepted' THEN 1 ELSE 0 END) / COUNT(submission_id) DESC) AS rank
+                        SUM(CASE WHEN status = 'Accepted' THEN 1 ELSE 0 END) / COUNT(submission_id) AS pass_rate
                     FROM
                         submit_records
                     WHERE
@@ -27,13 +27,16 @@ def get_rank():
                         student_id
                     ORDER BY
                         pass_rate DESC
+
                 """)
 
         # 执行查询
         res = session.execute(query)
 
+        df_res = toDataFrame(res)
+        print(df_res)
         # 将查询结果转换为JSON
-        json_res = toJSON(res)
+        json_res = df_res.to_json(orient='records')
 
         # 返回JSON响应
         return json_res
